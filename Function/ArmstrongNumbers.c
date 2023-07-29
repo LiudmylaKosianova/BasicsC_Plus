a0e59fb4c5b028e15aa737aca79e1e6ee710dcba
snowwhitesheep <snw.wht.lk@gmail.com>
2023-07-29 07:11:59 +0200

    start

---
+++ Function/ArmstrongNumbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+//takes two int between 100 and 999 as an input
+//finds armstrong numbers between them
+//armstrong number - sum of cubes of its digits equals to number
+
+int main(){
+
+    return 0;
+}
