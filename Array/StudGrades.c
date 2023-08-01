bf18e8d66460d71db0364cb41c389d71dcf636a9
snowwhitesheep <snw.wht.lk@gmail.com>
2023-08-01 14:53:58 +0200

    start

---
+++ Array/StudGrades.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//assigns grades to students based on their average scores
+double getAverageScore(double array[5]);
+int computeGrade(int a);
+int main(){
+    return 0;
+}
+
+double getAverageScore(double array[5]){
+    double sum = 0;
+    
+    return sum/5;
+}
