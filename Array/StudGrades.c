175f47191206ec24e1cb2d786109fd7a6f466ba8
snowwhitesheep <snw.wht.lk@gmail.com>
2023-08-01 14:59:43 +0200

    halfway

--- Array/StudGrades.c
+++ Array/StudGrades.c
@@ -1,13 +1,24 @@
 #include<stdio.h>
 //assigns grades to students based on their average scores
 double getAverageScore(double array[5]);
-int computeGrade(int a);
+char computeGrade(double a);
 int main(){
+
+    double scores[5] = {55, 64, 75, 80, 65};
+ 
+    double averageScore = getAverageScore(scores);
+    printf("%.2lf", averageScore);
+
     return 0;
 }
 
 double getAverageScore(double array[5]){
     double sum = 0;
-    
+    for (int i = 0; i < 5; i++){
+        sum += array[i];
+    }
     return sum/5;
+}
+char computeGrade(double a){
+    
 }
